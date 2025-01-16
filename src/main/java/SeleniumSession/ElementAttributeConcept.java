da645231268ae07685b63b66944b828e0eeb9af0
AmritaDinda <amritadinda84@gmail.com>
2025-01-16 18:49:05 +0530

    Added Selenium

--- src/main/java/SeleniumSession/ElementAttributeConcept.java
+++ src/main/java/SeleniumSession/ElementAttributeConcept.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ElementAttributeConcept {
 	
-	static WebDriver driver;
+static WebDriver driver;
 
 	public static void main(String[] args) {
 		driver = new ChromeDriver();
